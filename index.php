4dae4a11a0fd1ff5b7faf21c33b71f12d81573ca
enricoforesta <enrico.foresta97@gmail.com>
2024-01-10 14:40:13 +0100

    feat: boostrap + form

--- index.php
+++ index.php
@@ -4,11 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- / BOOTSTRAP -->
+    <title>PHP Iscrizione Newsletter</title>
 </head>
 
 <body>
-
+    <div class="container-fluid">
+        <form class=" d-flex align-items-center justify-content-around " action="index.php" method="get">
+            <div>
+                <label for="email">Inserisci la tua email</label><br>
+                <input type="text" id="email" name="email">
+            </div>
+            <input id="submit" type="submit" value="Login">
+        </form>
+    </div>
 </body>
 
 </html>
